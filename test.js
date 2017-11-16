df16379b7f3aba433c3ce25aaf7958a0b8e1693b
Adrian Emil Grigore <adrian.emil.grigore@gmail.com>
2017-11-16 16:27:47 +0200

    Refactor test

--- test.js
+++ test.js
@@ -10,62 +10,77 @@ if (!Object.values)
 var slice = Array.prototype.slice;
 
 const data = {firstKey: 1, secondKey: 2};
+const request = {};
 
-test(function(t) {
-	t.plan(9);
-
-	const request = {};
+test('handles context, arguments and query', function(t) {
+	t.plan(2);
 
 	dispatch({
 		value: function(ctx) {
-			t.deepEqual(ctx, data);
+			t.deepEqual(ctx, data, 'context received');
 			t.deepEqual(Object.values(slice.call(arguments ,1)), [
 				'a',
 				'b',
 				'd=e',
 				request
-			]);
+			], 'arguments and query received');
 		},
 		args: [ 'a', 'b' ],
 		query: 'd=e'
 	}, data, request);
+});
+
+test('handles empty arguments array', function(t) {
+	t.plan(2);
 
 	dispatch({
 		value: function(ctx) {
-			t.deepEqual(ctx, data);
+			t.deepEqual(ctx, data, 'context received');
 			t.deepEqual(Object.values(slice.call(arguments ,1)), [
 				'd=e',
 				request
-			]);
+			], 'query received');
 		},
 		args: [],
 		query: 'd=e'
 	}, data, request);
+});
+
+test('handles empty query', function(t) {
+	t.plan(2);
 
 	dispatch({
 		value: function(ctx) {
-			t.deepEqual(ctx, data);
-			t.deepEqual(Object.values(slice.call(arguments ,1)), [
-				'',
-				request
-			]);
-		},
-		args: [],
-		query: ''
-	}, data, request);
-
-	dispatch({
-		value: function(ctx) {
-			t.deepEqual(ctx, data);
+			t.deepEqual(ctx, data, 'context received');
 			t.deepEqual(Object.values(slice.call(arguments ,1)), [
 				'a',
 				'',
 				request
-			]);
+			], 'arguments and empty query received');
 		},
 		args: [ 'a' ],
 		query: ''
 	}, data, request);
+});
+
+test('handles empty arguments array and empty query', function(t) {
+	t.plan(2);
+
+	dispatch({
+		value: function(ctx) {
+			t.deepEqual(ctx, data, 'context received');
+			t.deepEqual(Object.values(slice.call(arguments ,1)), [
+				'',
+				request
+			], 'empty arguments and empty query received');
+		},
+		args: [],
+		query: ''
+	}, data, request);
+});
+
+test('throws error non function match value', function(t) {
+	t.plan(1);
 
 	t.throws(function(){
 		dispatch({
@@ -73,30 +88,22 @@ test(function(t) {
 			args: [],
 			query: ''
 		}, request);
-	}, /Route's match value must be a function/);
+	}, /Route's match value must be a function/, 'error thrown');
 });
 
-test('Support callbacks', function(t) {
-	t.plan(4);
+test('dispatches response', function(t) {
+	t.plan(2);
 
 	const request = {};
 
 	dispatch({
-		value: function(ctx, a, b, query, request) {
-			t.deepEqual(ctx, data);
-			t.deepEqual(Object.values(slice.call(arguments ,1)), [
-				'a',
-				'b',
-				'd=e',
-				request
-			]);
-
+		value: function() {
 			return 'result';
 		},
 		args: [ 'a', 'b' ],
 		query: 'd=e'
 	}, data, request, function( err, result ){
-		t.notOk(err);
-		t.equal(result, 'result');
+		t.notOk(err, 'no error returned');
+		t.equal(result, 'result', 'response dispatched');
 	});
 });
